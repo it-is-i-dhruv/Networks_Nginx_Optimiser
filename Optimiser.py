11e4450c724dadced1af2ed0e291ae4b2530bd29
it-is-i-dhruv <thedevdhruv@proton.me>
2024-04-21 23:29:38 +0800

    Finalised script, updated configs with all optimisations

--- Optimiser.py
+++ Optimiser.py
@@ -2,10 +2,8 @@ import os
 import subprocess
 import re
 
-# path = os.chdir('/etc/nginx')
-
 opt_params = {"worker_processes", "tcp_nopush", "keepalive_timeout", "keepalive_requests", "listen", "events"}
-
+print("Nginx Optimisation started.")
 def worker_processes_opt(work, conf):
     print("<---- worker process found, triggering function worker_process_opt --->")
     # print(work)
@@ -18,8 +16,6 @@ def worker_processes_opt(work, conf):
     head = subprocess.run(['head', '-1'], input=grep_cores.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     cores_line= head.stdout.decode('utf-8')
     cores = cores_line.strip().split()[3]
-    # print(cores)
-    # print(type(cores))
     work_split[1] = '{};\n'.format(cores)
     # print(work_split)
     final_line = ' '.join(work_split)
@@ -28,21 +24,20 @@ def worker_processes_opt(work, conf):
     conf.append(final_line)
     return None
 def events_opt(events, conf):
-    print("events triggered")
+    print("<--- starting multi accept optimisations --->")
     conf.append(events)
     multi_line = "multi_accept on;\n"
     conf.append(multi_line)
-    print("<---- finished multi accept --->")
+    print("<---- finished multi accept optimisations --->")
     return None
 
 def listen_opt(listen, conf):
-    print("listen triggered")
+    print("<---- starting reuseport (socket sharding) optimisations --->")
     listen_split = listen.strip().split()
-    print(listen_split[0])
-    print(listen_split[1])
     listen_split[1] = '80 reuseport;\n'
     final_line = ' '.join(listen_split)
     conf.append(final_line)
+    print("<---- finished reuseport (socket sharding) optimisations --->")
 
     return None
 def modify_nginx_config(file_path):
@@ -167,32 +162,16 @@ with open(file_path, 'r+') as config:
             # print(i.strip().split())
             # print(i.strip().split()[0][1:])
             if i.strip().split()[0] in opt_params   :
-                # print("i:", i)
-                # print("comments: ", comments)
-                # print(globals())
-                # worker_process_opt(i, comments)
-                # print(i.strip().split()[0]+ "_opt")
                 globals()[i.strip().split()[0]+ "_opt"](i, comments)
                 continue
             elif i.strip().split()[0][1:] in opt_params: 
-                # print("i:", i)
-                # print("comments: ", comments)
-                # print(globals())
-                # # worker_process_opt(i, comments)
-                # print(i.strip().split()[0]+ "_opt")
                 globals()[i.strip().split()[0][1:]+ "_opt"](i, comments)
                 continue
-            # if "worker_processes" in i:
-            #     worker_process_opt(i) 
-            # if i[-2] == ';':
-            #     # print(i)
-            #     continue
             comments.append(i)
         except:
             comments.append(i)
             continue
-    # print(comments)
-    # assert comments == lines
+
     ssl_updated_lines = update_ssl_directives(comments)
     config.seek(0)
     config.truncate(0)
@@ -201,16 +180,11 @@ with open(file_path, 'r+') as config:
 
 modify_nginx_config(file_path)
 #at the end just replace conf with comments
-#check for nginx -t
-# result = subprocess.run(['nginx', '-t'], stdout=subprocess.PIPE)
-# checker = str(result.stdout.decode('utf-8'))
-# print(checker)
-# assert "is successful" in checker
 result = subprocess.run(['nginx', '-t'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout_str = result.stdout.decode('utf-8')
 stderr_str = result.stderr.decode('utf-8')
 
-# print(stdout_str)
-print(stderr_str)
 #no idea why it outputs to stderr instead of stdout......
 assert "is successful" in stderr_str
+print("Nginx Optimisation Completed.")
+print('Have a nice day!')
